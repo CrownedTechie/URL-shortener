b3ca3656bcf075212c8659215b8ee1f15e8d9774
Crownedtechie <nenchi00@gmail.com>
2026-02-16 21:11:14 +0100

    setup:: verify server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config({ path: "./config.env" });
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+	console.log(`App running on port ${port}...`);
+});
